de8674027d1949665bafbdec7d843d648a3a1898
Ayushkumar Maurya <ayushskmaurya@gmail.com>
2021-01-29 18:34:28 +0530

    Update User Password

--- userQuestion.php
+++ userQuestion.php
@@ -1,57 +1,12 @@
-<?php
-	include "connection.php";
-
-	if(isset($_POST['submit'])) {
-		$category = $_POST['category'];
-		$topic = trim($_POST['topic']);
-		$question = trim($_POST['question']);
-		
-		$sql = "INSERT INTO userquestion(category, topic, question, email) values(:category, :topic, :question, :email)";
-		$stmt = $conn->prepare($sql);
-		$stmt->execute(array(
-			':category' => $category,
-			':topic' => $topic,
-			':question' => $question,
-			':email' => $_SESSION['email']
-		));
-	
-		echo "<script>alert('Your question has been sent to our SME for review.');</script>";
-
-		$subject = "Question has been sent to our SME";
-		$message = 'Your question has been sent to our SME for review.';
-
-		$header = "MIME-Version: 1.0 \r\n";
-		$header .= "Content-Type: text/html; charset=UTF-8 \r\n";
-		
-		mail($_SESSION['email'], $subject, $message, $header);
-	}
-?>
-
-<script>
-	function validate() {
-		var topic = document.getElementById("topic").value.trim();
-		var question = document.getElementById("question").value.trim();
-		
-		if(topic.length == 0) {
-			document.getElementById("error").innerHTML = "Please fill out Topic field.";
-			document.getElementById("error").style.display = "block";
-			return false;
-		}
-		else if(question.length == 0) {
-			document.getElementById("error").innerHTML = "Please fill out question field.";
-			document.getElementById("error").style.display = "block";
-			return false;
-		}
-		return true;
-	}
-</script>
+<?php include "connection.php"; ?>
+<script src="js/jquery-2.2.4.min.js"></script>
 
 <html>
 	<body>
-		<form onsubmit="return validate()" action="/Affable/userQuestion.php" method="post">
-			<input type="text" name="client-email" placeholder="<?= $_SESSION['email'] ?>" disabled>
+		<form>
+		<input type="text" name="client-email" placeholder="<?= $_SESSION['email'] ?>" disabled>
 			<br><br>
-			<select name="category">
+			<select id="category" name="category">
 			<?php
 				$stmt = $conn->prepare("SELECT categoryName FROM category ORDER BY categoryName");
 				$stmt->execute();
@@ -66,8 +21,43 @@
 			<br><br>
 			<input type="checkbox" onchange="document.getElementById('submit').disabled = !this.checked;" name="confirm" value="confirm">By checking this you confirm to consult our SME
 			<br><br>
-			<input type="submit" id="submit" name="submit" value="Post your question" disabled>
-			<div id="error" style="display: none;"></div>
+			<input type="button" id="submit" name="submit" value="Post your question" disabled>
+			<div id="status" style="display: none;"></div>
 		</form>
 	</body>
+
+	<script>
+		$(document).ready(function() {
+			$('#submit').click(function() {
+				var category = document.getElementById("category").value;
+				var topic = document.getElementById("topic").value.trim();
+				var question = document.getElementById("question").value.trim();
+				var error = 0;
+
+				if(topic.length == 0)
+					error = "Please fill out Topic field."
+				else if(question.length == 0)
+					error = "Please fill out question field."		
+				
+				if(error != 0) {
+					document.getElementById("status").innerHTML = error;
+					document.getElementById("status").style.display = "block";
+				}
+				
+				else {
+					$.ajax({
+						url: "userQuestion_entry.php",
+						method: "POST",
+						data: {category:category, topic: topic, question: question},
+						success: function(status) {
+							if(status == 1) {
+								document.getElementById("status").innerHTML = "Your question has been sent to our SME for review.";
+								document.getElementById("status").style.display = "block";
+							}
+						}
+					});
+				}
+			});
+		});
+	</script>
 </html>
---
+++ userQuestion_entry.php
@@ -0,0 +1,25 @@
+<?php
+	include "connection.php";
+
+	$category = $_POST['category'];
+	$topic = $_POST['topic'];
+	$question = $_POST['question'];
+	
+	$sql = "INSERT INTO userquestion(category, topic, question, email) values(:category, :topic, :question, :email)";
+	$stmt = $conn->prepare($sql);
+	$stmt->execute(array(
+		':category' => $category,
+		':topic' => $topic,
+		':question' => $question,
+		':email' => $_SESSION['email']
+	));
+
+	$subject = "Question has been sent to our SME";
+	$message = 'Your question has been sent to our SME for review.';
+
+	$header = "MIME-Version: 1.0 \r\n";
+	$header .= "Content-Type: text/html; charset=UTF-8 \r\n";
+	
+	mail($_SESSION['email'], $subject, $message, $header);
+	echo 1;
+?>
